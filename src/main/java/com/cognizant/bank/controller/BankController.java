17379b6b2b584190d77d8a261ec1ff864b15628f
Erisunya <erissunya@gmail.com>
2023-01-09 18:59:27 +0800

    Initial commit

---
+++ src/main/java/com/cognizant/bank/controller/BankController.java
@@ -0,0 +1,36 @@
+package com.cognizant.bank.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.cognizant.bank.models.UserRequest;
+
+@RestController
+public class BankController {
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@RequestBody UserRequest userRequest) {
+		
+		String uri = "http://localhost:8084/signup";
+		RestTemplate restTemplate = new RestTemplate();
+		
+		return restTemplate.postForEntity(uri, userRequest, ResponseEntity.class);
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@RequestBody UserRequest userRequest) {
+		
+		String uri = "http://localhost:8084/login";
+		RestTemplate restTemplate = new RestTemplate();
+		
+		return restTemplate.postForEntity(uri, userRequest, ResponseEntity.class);
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/cognizant/bank/models/UserRequest.java
@@ -0,0 +1,27 @@
+package com.cognizant.bank.models;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequest {
+	
+	private String name;
+	private String username;
+	private String password;
+	private String address;
+	private String country;
+	private String email;
+	private long accountNo;
+	private long contactNo;
+	private Date dob;
+	private String accountType;
+	private String role;
+	
+	
+}
